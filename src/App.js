9382c9e1c530aeb3c7bc09434649767285e8b8d7
Anurag Khanna <anuragkhanna@iiitmanipur.ac.in>
2023-02-01 05:46:11 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  let [account, setAccount] = useState("");
+  let [contractData, setContractData] = useState("");
+
+  const { ethereum } = window;
+  const connectMetamask = async () => {
+    if (window.ethereum !== "undefined"){
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      setAccount(accounts[0]);
+  }
+  }
+
+  const connectContract = async () => {
+    return;
+  }
+  const getData = async () => {
+    return;
+  }
+  const changeData = async () => {
+    return;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <button onClick={connectMetamask}>CONNECT TO METAMASK</button>
+          <p>{account}</p>
+          <button onClick={connectContract}>CONNECT TO CONTRACT</button><br/ > <br/ >
+          <button onClick={changeData}>CHANGE DATA</button><br/ > <br/ >
+          <button onClick={getData}>READ FROM CONTRACT</button>
+          <p>{contractData}</p>
     </div>
   );
 }
-
 export default App;
